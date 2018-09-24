43b6aeb198b6c3321eba3f795e8a06d99df66647
stephane tallard <stephane.tallard@capgemini.com>
2018-09-24 10:10:53 +0200

    Indus drci was here

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -27,7 +27,7 @@ public class InsultGenerator {
 					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1) {
 						article = "a";
 					}
-					theInsult = String.format("Thou art %s %s %s %s!", article, rs.getString("first"),
+					theInsult = String.format("Indus DRCI was here > Thou art %s %s %s %s!", article, rs.getString("first"),
 							rs.getString("second"), rs.getString("noun"));
 				}
 				rs.close();
